dde29e0bff5bb4d4889583998fd3e5a4a03fc75b
Jean-Eudes <eudes.agbre@gmail.com>
2023-12-31 00:34:51 +0100

    Adding the . in the instruction regarding how to execute the pip install -e .; The . is compulsory otherwise it wont work.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # This file will be used to install the packages.
-# The command `pip install -e` should be executed in the terminal while being in the directory of this file.
+# The command `pip install -e .` should be executed in the terminal while being in the directory of this file.
 
 from setuptools import setup, find_packages
 
