67232048bebb33f2f3d3efbf476694b48d2d47bc
Jean-Eudes <eudes.agbre@gmail.com>
2023-12-31 00:25:00 +0100

    Adding comments and instructions in the setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# This file will be used to install the packages.
+# The command `pip install -e` should be executed in the terminal while being in the directory of this file.
+
 from setuptools import setup, find_packages
 
 setup(
